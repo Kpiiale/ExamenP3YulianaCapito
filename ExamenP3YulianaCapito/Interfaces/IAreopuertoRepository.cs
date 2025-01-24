8201e592cb7de9c8babefb119b305e8c1be6a1c2
Kpiiale <yuliana.capito@udla.edu.ec>
2025-01-24 07:35:32 -0500

    Interfaz y repositorio incompletos

---
+++ ExamenP3YulianaCapito/Interfaces/IAreopuertoRepository.cs
@@ -0,0 +1,15 @@
+﻿using ExamenP3YulianaCapito.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3YulianaCapito.Interfaces
+{
+    internal interface IAreopuertoRepository
+    {
+        List<AreopuertoYC> DevuelveListaAreopuertos();
+        AreopuertoYC DevuelveInfoAreopuerto(int Id);
+    }
+}
---
+++ ExamenP3YulianaCapito/Repositories/AreopuertoRepository.cs
@@ -0,0 +1,31 @@
+﻿using ExamenP3YulianaCapito.Interfaces;
+using ExamenP3YulianaCapito.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json; 
+
+namespace ExamenP3YulianaCapito.Repositories
+{
+    internal class AreopuertoRepository : IAreopuertoRepository
+    {
+        public HttpClient _httpClient;
+        public string endpoint = "https://www.freetestapi.com/api/v1/airports?search=Singapore";
+
+        public AreopuertoRepository()
+        {
+            _httpClient = new HttpClient(); 
+        }
+        public AreopuertoYC DevuelveInfoAreopuerto(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AreopuertoYC> DevuelveListaAreopuertos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
