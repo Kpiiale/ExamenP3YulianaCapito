2929e0e38f3b85ab6b7305660a08a3f331e2a7d3
Kpiiale <yuliana.capito@udla.edu.ec>
2025-01-24 11:10:31 -0500

    Creacion del repositorio de sqlite

--- ExamenP3YulianaCapito/Models/HistorialYC.cs
+++ ExamenP3YulianaCapito/Models/HistorialYC.cs
@@ -14,6 +14,6 @@ namespace ExamenP3YulianaCapito.Models
         public int Id { get; set; } 
         public string Name { get; set; }
         public string City { get; set; }
-        public string Country { get; set; } /
+        public string Country { get; set; }
     }
 }
---
+++ ExamenP3YulianaCapito/Repositories/AeropuertoSQLiteRepository.cs
@@ -0,0 +1,18 @@
+﻿using ExamenP3YulianaCapito.Interfaces;
+using ExamenP3YulianaCapito.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3YulianaCapito.Repositories
+{
+    public class AeropuertoSQLiteRepository : IAreopuertoRepository
+    {
+        public List<AreopuertoYC> BuscarAreopuertos(string busqueda)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
