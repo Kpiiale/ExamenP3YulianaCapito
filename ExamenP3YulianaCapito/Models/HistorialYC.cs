4b0a9740159ea830519838cf28117b1ef25234f0
Kpiiale <yuliana.capito@udla.edu.ec>
2025-01-24 09:26:50 -0500

    Conenzando con la segunda pestana

---
+++ ExamenP3YulianaCapito/Models/HistorialYC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL; 
+
+namespace ExamenP3YulianaCapito.Models
+{
+    public class HistorialYC
+    {
+        [PrimaryKe]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; } /
+    }
+}
