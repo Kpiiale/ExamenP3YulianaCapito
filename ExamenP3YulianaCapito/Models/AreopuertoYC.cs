5f1f738ad315937095ba9d1fb7796c571fef04e7
Kpiiale <yuliana.capito@udla.edu.ec>
2025-01-24 07:22:48 -0500

    Modelo

---
+++ ExamenP3YulianaCapito/Models/AreopuertoYC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3YulianaCapito.Models
+{
+    internal class AreopuertoYC
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Longitude { get; set; } 
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
